d10dc0c8773b70d722d0192ad74a775d55179be4
Oscar Pettersson <pson93@gmail.com>
2025-07-22 12:53:52 +0200

    push

---
+++ challenge.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+BOOKS = [
+    {"title": "Title One", "author": "Author One", "category": "Science"},
+    {"title": "Title Two", "author": "Author Two", "category": "Science"},
+    {"title": "Title Three", "author": "Author Three", "category": "History"},
+    {"title": "Title Four", "author": "Author Four", "category": "Math"},
+    {"title": "Title Five", "author": "Author Five", "category": "Math"},
+    {"title": "Title Six", "author": "Author Two", "category": "Math"},
+]
+
+@app.get("/author/{author}")
+async def fetch_book(author: str):
+    
+    results = [
+        book for book in BOOKS if
+        book["author"].casefold() == author.casefold()
+    ]
+    if not results:
+        raise HTTPException(status_code=404, detail="no book was found")
+    return results
+
